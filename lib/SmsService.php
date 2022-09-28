18f9d33c0419671b7ae022028f33376559bd7927
Nguyen Truong Giang <giang.nguyen@diag.vn>
2022-09-28 19:31:37 +0700

    Update package

--- lib/SmsService.php
+++ lib/SmsService.php
@@ -3,6 +3,8 @@
 namespace DiagVN;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class SmsService
 {
@@ -24,10 +26,13 @@ class SmsService
                 Log::debug("Phone: " . $number, ['message' => $message, 'FPT Response' => $response]);
             }
 
+            $this->logSms($number, $response);
+
             return $response;
         } catch (\Exception $ex) {
             Log::error("Can not send message FPT: " . $number, ['message' => $message, 'error' => $ex->getMessage()]);
             report($ex);
+            $this->logSms($number, $response, false);
             return [];
         }
     }
@@ -40,5 +45,19 @@ class SmsService
         }
 
         return true;
+    }
+
+    private function logSms(
+        string $number,
+        array $response,
+        bool $isSuccess = true,
+    ) {
+        DB::table('sms_logs')->insert([
+            'phone_number' => $number,
+            'is_success' => $isSuccess,
+            'response' => json_encode($response),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
